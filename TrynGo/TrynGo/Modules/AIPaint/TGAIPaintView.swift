b875910b2cd3b83267f230f4896c0dce6187db06
zhouyang <583524614@qq.com>
2023-11-28 02:44:22 +0800

    feat 整体框架完成

--- TrynGo/TrynGo/Modules/AIPaint/TGAIPaintView.swift
+++ TrynGo/TrynGo/Modules/AIPaint/TGAIPaintView.swift
@@ -11,10 +11,16 @@ struct TGAIPaintView : View {
     let bgColor = Color(hex: 0xd0d0d0).opacity(0.2)
     let left = 96.0
     let styleWidth = 150.0
-    let texts = ["选择画布尺寸", "1 : 1",
-                 "2 : 3" , "3 : 2",
-                 "3 : 4" , "4 : 4",
-                 "9 : 16", "16 : 9"]
+    let sizeRatioTitle = ["选择画布尺寸", "1 : 1",
+                          "2 : 3" , "3 : 2",
+                          "3 : 4" , "4 : 3",
+                          "9 : 16", "16 : 9"]
+    let sizeRatioPic   = ["", "ai_border_1_1",
+                          "ai_border_2_3", "ai_border_3_2",
+                          "ai_border_3_4", "ai_border_4_3",
+                          "ai_border_9_16", "ai_border_16_9"]
+    let sizeRatio: [CGFloat] = [1.0, 2/3, 3/2, 3/4, 4/3, 9/16, 16/9]
+    
     let styleArray = ["ai_style_p1", "ai_style_p2", "ai_style_p3", "ai_style_p4", "ai_style_p5", "ai_style_p6"]
     let sceneArray = ["ai_scene_p1", "ai_scene_p2", "ai_scene_p3", "ai_scene_p4", "ai_scene_p5", "ai_style_p6"]
     @State private var selectedStyleIndex = -1
@@ -24,13 +30,19 @@ struct TGAIPaintView : View {
     
     @State private var selectedImage: UIImage?
     @State private var isImagePickerPresented = false
-    @State private var isSelectedIndex: Int = 1
+    @State private var selectedRatioIndex: Int = 1
     
     @State private var showStyleSheet = false  // 风格卡片
     @State private var showScenceSheet = false // 场景卡片
     
     @State private var userInput: String = ""
     @FocusState private var onFocused : Bool
+    
+    private func calculateW(_ w: CGFloat, _ h: CGFloat) -> CGFloat {
+        let ratio = sizeRatio[selectedRatioIndex-1]
+        let width = (w - h * ratio) / 2
+        return width
+    }
     
     var body: some View {
         VStack (alignment: .center, content: {
@@ -45,21 +57,29 @@ struct TGAIPaintView : View {
                                 .resizable()
                                 .scaledToFit()
                                 .frame(maxWidth: .infinity, maxHeight:.infinity)
-                            Image("ai_pic_border")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(maxWidth: .infinity, maxHeight:.infinity)
-                        }
-                        else {
+                        } else {
                             Text("点 击 选 择 图 片")
                                 .frame(maxWidth: .infinity, maxHeight:.infinity)
                                 .font(.largeTitle)
                         }
+                        Image(sizeRatioPic[selectedRatioIndex])
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity, maxHeight:.infinity)
+                        GeometryReader { geometry in
+                            let width = calculateW(geometry.size.width, geometry.size.height)
+                            Color(hex: 0x969696).opacity(0.6)
+                                .frame(width: width, height: geometry.size.height)
+                                .offset(x: 0)
+                            Color(hex: 0x969696).opacity(0.6)
+                                .frame(width: width, height: geometry.size.height)
+                                .offset(x: geometry.size.width-width)
+                        }
                     }
                 }
                 .background(.white)
-                .cornerRadius(24)
-                .frame(width: ScreenWidth*0.5, height: ScreenHeight*0.4)
+                .cornerRadius(18)
+                .frame(width: ScreenWidth*0.5, height: ScreenWidth*9/32)
                 .onTapGesture {
                     isImagePickerPresented.toggle()
                 }
@@ -70,8 +90,8 @@ struct TGAIPaintView : View {
                 Spacer()
                 
                 LazyVGrid(columns: [GridItem(.fixed(180)), GridItem(.fixed(180), alignment: .leading)], spacing: 18) {
-                    ForEach(texts.indices, id: \.self) { index in
-                        let text = texts[index]
+                    ForEach(sizeRatioTitle.indices, id: \.self) { index in
+                        let text = sizeRatioTitle[index]
                         if index == 0 {
                             Text(text)
                                 .frame(width: 180, height: 50)
@@ -83,12 +103,12 @@ struct TGAIPaintView : View {
                             Text(text)
                                 .frame(width: 180, height: 50)
                                 .font(.custom("Helvetica", size: 24))
-                                .foregroundColor(isSelectedIndex == index ? Color.black : Color.white)
-                                .background(isSelectedIndex == index ? Color.white : bgColor)
+                                .foregroundColor(selectedRatioIndex == index ? Color.black : Color.white)
+                                .background(selectedRatioIndex == index ? Color.white : bgColor)
                                 .cornerRadius(32)
                                 .padding()
                                 .onTapGesture {
-                                    isSelectedIndex = index
+                                    selectedRatioIndex = index
                                 }
                         }
                     }
@@ -102,58 +122,10 @@ struct TGAIPaintView : View {
             // 风格卡片 + 场景卡片 + 描述画面 + 生成按钮
             HStack (alignment: .bottom, content: {
                 Spacer().frame(width: left)
-                VStack {
-                    Button("风格卡片") {
-                        showStyleSheet = true
-                    }
-                    .font(.title2)
-                    .background(Image("ai_style"))
-                    .frame(width: styleWidth, height: 45)
-                    .cornerRadius(16)
+                SenceCardView(selectedStyleIndex: $selectedStyleIndex, isShow: $showStyleSheet, styleArray: styleArray, title: "风格卡片", btnImage: "ai_style")
                     .padding()
-                    .sheet(isPresented: $showStyleSheet, content: {
-                        TGSheetGridView(images: styleArray, selectedIndex:$selectedStyleIndex)
-                    })
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 150, height: 150)
-                            .foregroundColor(bgColor)
-                            .cornerRadius(12)
-                        if selectedStyleIndex != -1 {
-                            Image(styleArray[selectedStyleIndex])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 150, height: 150)
-                        }
-                    }
-                }
-                .padding()
-                VStack {
-                    Button("场景卡片") {
-                        showScenceSheet = true
-                    }
-                    .font(.title2)
-                    .background(Image("ai_scene"))
-                    .frame(width: styleWidth, height: 45)
-                    .cornerRadius(16)
+                SenceCardView(selectedStyleIndex: $selectedSceneIndex, isShow: $showScenceSheet, styleArray: sceneArray, title: "场景卡片", btnImage: "ai_scene")
                     .padding()
-                    .sheet(isPresented: $showScenceSheet, content: {
-                        TGSheetGridView(images: sceneArray, selectedIndex:$selectedSceneIndex)
-                    })
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 150, height: 150)
-                            .foregroundColor(bgColor)
-                            .cornerRadius(12)
-                        if selectedSceneIndex != -1 {
-                            Image(sceneArray[selectedSceneIndex])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 150, height: 150)
-                        }
-                    }
-                }
-                .padding()
                 Spacer()
                 HStack (alignment: .bottom, content: {
                     TextEditor(text: $userInput)
@@ -286,5 +258,53 @@ struct AIGeneratingView : View {
 }
 
 #Preview {
-    AIGeneratingView()
+    TGAIPaintView()
+}
+
+struct SenceCardView: View {
+    @Binding var selectedStyleIndex : Int
+    @Binding var isShow :Bool
+    var styleArray : [String]
+    var title : String
+    var btnImage : String
+    let styleWidth = 150.0
+    
+    var body: some View {
+        VStack {
+            Button(title) {
+                isShow = true
+            }
+            .font(.title2)
+            .background(Image(btnImage))
+            .frame(width: styleWidth, height: 45)
+            .cornerRadius(16)
+            .padding()
+            .sheet(isPresented: $isShow, content: {
+                TGSheetGridView(images: styleArray, selectedIndex:$selectedStyleIndex)
+            })
+            ZStack {
+                Rectangle()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(bgColor)
+                    .cornerRadius(12)
+                if selectedStyleIndex != -1 {
+                    Image(styleArray[selectedStyleIndex])
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .overlay(alignment: .topTrailing) {
+                            Image("ai_close_small")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 24, height: 24)
+                                .offset(x: 5, y: -5)
+                                .onTapGesture {
+                                    selectedStyleIndex = -1
+                                }
+                        }
+                }
+            }
+        }
+        .padding()
+    }
 }
